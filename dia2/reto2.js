2ad3b79b600b53478cc188c809ae77761aad5a98
JaviFZ <jferzap@gmail.com>
2022-11-15 19:40:30 +0100

    subida de dia2 terminado

--- dia2/reto2.js
+++ dia2/reto2.js
@@ -19,45 +19,45 @@ connection.connect((error) => {
 
 // // OBTEN EL ID Y LA NOTA DE LOS ALUMNOS QUE TENGAN UN ID ENTRE 1 Y 20, O QUE SU NOTA SEA MAS DE 8 Y TENGA FECHA DEL AÑO PASADO
 
-let sql = "SELECT (mark) AND (id_studient) FROM marks WHERE id_subject BETWEEN '1' AND '20' OR WHERE mark > 8 AND date BETWEEN '2021-01-01' AND '2021-12-31'";
-connection.query(sql, function (err, result) 
-{
-    if(err)
-    console.log(err);
-    else
-    {
-        console.log("Actualizado");
-        console.log(result);
-    }    
-});
+// let sql = "SELECT mark , id_studient FROM marks WHERE id_studient BETWEEN 1 AND 20 OR WHERE mark > 8 AND date BETWEEN '2021-01-01' AND '2021-12-31'";
+// connection.query(sql, function (err, result) 
+// {
+//     if(err)
+//     console.log(err);
+//     else
+//     {
+//         console.log("Actualizado");
+//         console.log(result);
+//     }    
+// });
 
 
 // // OBTEN LA MEDIA DE LAS NOTAS QUE SE HAN DADO EL ULTIMO AÑO POR ASIGNATURA
 
-let sql1 = "SELECT AVG (mark) FROM marks WHERE date BETWEEN '2022-01-01' AND '2022-12-31' GROUP BY  id_subject";
-connection.query(sql1, function (err, result) 
-{
-    if(err)
-    console.log(err);
-    else
-    {
-        console.log("Actualizado");
-        console.log(result);
-    }    
-});
+// let sql1 = "SELECT AVG (mark) FROM marks WHERE date BETWEEN '2022-01-01' AND '2022-12-31' GROUP BY  id_subject";
+// connection.query(sql1, function (err, result) 
+// {
+//     if(err)
+//     console.log(err);
+//     else
+//     {
+//         console.log("Actualizado");
+//         console.log(result);
+//     }    
+// });
 
 
 // // OBTEN LA MEDIA DE LAS NOTAS QUE SE HAN DADO EN EL ULTIMO AÑO POR ALUMNO
 
 
-let sql2 = "SELECT AVG (mark) FROM marks WHERE date BETWEEN '2022-01-01' AND '2022-12-31' GROUP BY  id_studient";
-connection.query(sql2, function (err, result) 
-{
-    if(err)
-    console.log(err);
-    else
-    {
-        console.log("Actualizado");
-        console.log(result);
-    }    
-});
+// let sql2 = "SELECT AVG (mark) FROM marks WHERE date BETWEEN '2022-01-01' AND '2022-12-31' GROUP BY  id_studient";
+// connection.query(sql2, function (err, result) 
+// {
+//     if(err)
+//     console.log(err);
+//     else
+//     {
+//         console.log("Actualizado");
+//         console.log(result);
+//     }    
+// });
